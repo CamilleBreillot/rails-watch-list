95e16c7db0a46ea5271e4d59a50d17aa229df2cb
camille breillot <camille.breillot@gmail.com>
2021-11-12 16:01:42 +0100

    with stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,16 @@ require("@rails/activestorage").start()
 require("channels")
 import "bootstrap";
 
+// Stimulus import
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+
+// Stimulus
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +26,5 @@ import "bootstrap";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
+  has_many :reviews
   has_many :movies, through: :bookmarks
   validates :name, uniqueness: true, presence: true
   has_one_attached :photo
